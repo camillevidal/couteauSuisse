2d32c03e7158f5da9a18ce09a86cba46535e1e88
Camille_VIDAL <camillevidal58@aol.fr>
2020-01-14 17:19:18 +0100

    package

---
+++ src/main/java/com/mycompany/couteausuisse/MergePdf.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.couteausuisse;
+
+
+import com.itextpdf.kernel.pdf.PdfReader;
+
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergePdf {
+
+    
+}
